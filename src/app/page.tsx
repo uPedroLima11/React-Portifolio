2e7efeba82c3deb20f4aeb0831edb6847ed67d89
Pedro Lima <pedromendeslima2016@gmail.com>
2024-10-21 20:53:50 -0300

    edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 import Header from "./components/header";
 import Image from "next/image";
 import { useState } from "react";
-import { Projeto, projetos} from "./components/projeto"
+import { Projeto, projetos } from "./components/projeto"
 import Footer from "./components/footer";
 const Home = () => {
-  
+
   const [selectedProject, setSelectedProject] = useState<Projeto | null>(null);
 
   const handleOpenModal = (projeto: Projeto) => setSelectedProject(projeto);
   const handleCloseModal = () => setSelectedProject(null);
 
-  
+
   return (
     <div>
       <header>
@@ -21,7 +21,7 @@ const Home = () => {
       <section className="flex items-center justify-between py-48 px-6 md:px-32 space-y-10 w-full">
         <div className="-mt-40 px-6 text-7xl font-light ">
           <h1 className="text-7xl mt-40 font-light"> Olá,</h1> <br />
-          <h2 className="-mt-16">Sou <span className="font-semibold">Pedro</span> <span className=" text-5xl mb-10 animate-fade">|</span> </h2>
+          <h2 className="-mt-16 flex items-center">Sou <span className="font-semibold ml-4">Pedro</span> <span className=" text-5xl mb-10 animate-fade relative translate-y-5 ml-3">|</span> </h2>
           <h1 className="text-lg mt-4">✨ Desenvolvedor Front-End ✨
             Focado em criar interfaces intuitivas <br />e responsivas, costumo utilizar React, TypeScript e Tailwind CSS para transformar
             <br />minhas ideias  em experiências digitais fluidas. </h1>
@@ -38,7 +38,7 @@ const Home = () => {
       </div>
       <section className="p-12">
         <div className="flex justify-center"> <h1 className="text-3xl font-bold">Sobre Mim</h1></div>
-        <div className="mt-32 ml-12"> <Image src="/Logos/linguagens.png" alt="sobremim"  width="450" height="400" className="rounded-xl border-4 border-[#101013]" />
+        <div className="mt-32 ml-12"> <Image src="/Logos/linguagens.png" alt="sobremim" width="450" height="400" className="rounded-xl border-4 border-[#101013]" />
         </div>
         <div className="flex justify-end -mt-[312px] mr-32">
           <h1 className="rounded-lg font-light bg-[#1a1a1f] -mr-24 p-14 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] ">
@@ -52,134 +52,133 @@ const Home = () => {
         </div>
       </section>
       <section className="mt-16">
-      <h1 className="text-center text-3xl font-bold">Tech Stack</h1>
+        <h1 className="text-center text-3xl font-bold">Tech Stack</h1>
 
-      <div className="mt-32 flex lg:flex-wrap items-center justify-center gap-20 px-4 lg:px-16 md:grid-cols-2 xl:grid-cols-4 ">
-      <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
-        <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-blue"></div>
-        <Image src="/Logos/typescript.png" alt="typescript" width={60} height={40} className="mt-20" />
-        <p className="text-white font-bold mt-10">TypeScript</p>
-      </div>
+        <div className="mt-32 flex lg:flex-wrap items-center justify-center gap-20 px-4 lg:px-16 md:grid-cols-2 xl:grid-cols-4 ">
+          <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
+            <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-blue"></div>
+            <Image src="/Logos/typescript.png" alt="typescript" width={60} height={40} className="mt-20" />
+            <p className="text-white font-bold mt-10">TypeScript</p>
+          </div>
 
-      <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
-        <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-cyan"></div>
-        <Image src="/Logos/react.png" alt="react" width={60} height={40} className="mt-20" />
-        <p className="text-white font-bold mt-10">React</p>
-      </div>
+          <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
+            <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-cyan"></div>
+            <Image src="/Logos/react.png" alt="react" width={60} height={40} className="mt-20" />
+            <p className="text-white font-bold mt-10">React</p>
+          </div>
 
-      <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
-        <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-yellow"></div>
-        <Image src="/Logos/javascript.png" alt="javascript" width={60} height={40} className="mt-20" />
-        <p className="text-white font-bold mt-10">JavaScript</p>
-      </div>
+          <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
+            <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-yellow"></div>
+            <Image src="/Logos/javascript.png" alt="javascript" width={60} height={40} className="mt-20" />
+            <p className="text-white font-bold mt-10">JavaScript</p>
+          </div>
 
-      <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
-        <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-green"></div>
-        <Image src="/Logos/tailwind.png" alt="tailwind" width={60} height={40} className="mt-20" />
-        <p className="text-white font-bold mt-10">Tailwind CSS</p>
-      </div>
-      </div>
+          <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
+            <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-green"></div>
+            <Image src="/Logos/tailwind.png" alt="tailwind" width={60} height={40} className="mt-20" />
+            <p className="text-white font-bold mt-10">Tailwind CSS</p>
 
-      <div className="mt-32 flex lg:flex-wrap items-center justify-center gap-20 px-4 lg:px-16 md:grid-cols-2 xl:grid-cols-4 ">
-      <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
-        <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-blue"></div>
-        <Image src="/Logos/mysql1.png" alt="mysql1" width={60} height={40} className="mt-20" />
-        <p className="text-white font-bold mt-10">MySQL</p>
-      </div>
+          </div></div>
 
-      <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
-        <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-cyan"></div>
-        <Image src="/Logos/python.png" alt="react" width={60} height={40} className="mt-20" />
-        <p className="text-white font-bold mt-10">Python</p>
-      </div>
+        <div className="mt-32 flex lg:flex-wrap items-center justify-center gap-20 px-4 lg:px-16 md:grid-cols-2 xl:grid-cols-4 ">
+          <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
+            <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-blue"></div>
+            <Image src="/Logos/mysql1.png" alt="mysql1" width={60} height={40} className="mt-20" />
+            <p className="text-white font-bold mt-10">MySQL</p>
+          </div>
 
-      <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
-        <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-black"></div>
-        <Image src="/Logos/git.png" alt="nodejs" width={60} height={40} className="mt-20" />
-        <p className="text-white font-bold mt-10">NextJS</p>
-      </div>
-      <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
-        <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-red"></div>
-        <img src="/Logos/wireframe.png" alt="test" width={60} height={40} className="mt-20" />
-        <p className="text-white font-bold mt-10">Git</p>
-      </div>
-      <div id="projetos"></div>   </div>
-          
+          <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
+            <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-cyan"></div>
+            <Image src="/Logos/python.png" alt="react" width={60} height={40} className="mt-20" />
+            <p className="text-white font-bold mt-10">Python</p>
+          </div>
+
+          <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
+            <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-black"></div>
+            <Image src="/Logos/git.png" alt="nodejs" width={60} height={40} className="mt-20" />
+            <p className="text-white font-bold mt-10">NextJS</p>
+          </div>
+          <div className="relative pb-24 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 px-4 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-lg flex-grow overflow-hidden">
+            <div className="absolute inset-0 border-2 border-transparent rounded-lg loading-border-red"></div>
+            <img src="/Logos/wireframe.png" alt="test" width={60} height={40} className="mt-20" />
+            <p className="text-white font-bold mt-10">Git</p>
+          </div></div>
+          <div className="-mt-24" id="projetos"></div>
       </section>
-
       
-      <section className="mt-16">
-      <h1 className="text-center text-3xl font-bold">Alguns Projetos Desenvolvidos</h1>
 
-      <div className="mt-28 grid grid-cols-2 gap-20 lg:px-16">
-        {projetos.map((projeto, index) => (
-          <div
-            key={index}
-            className="relative pb-11 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-2xl overflow-hidden"
-          >
-            <Image
-              src={projeto.imagem}
-              alt={projeto.nome}
-              width={400}
-              height={400}
-              className="mt-8 w-[600px]"
-            />
-            <p className="text-white text-xl font-bold mt-10">{projeto.nome}</p>
+      <section className="mt-40">
+        <h1 className="text-center text-3xl font-bold">Alguns Projetos Desenvolvidos</h1>
 
-            <div className="flex gap-4 mt-6">
-              <Link href={projeto.githubLink} target="_blank">
-                <button className="py-2 px-4 bg-gray-300 font-semibold text-black rounded-lg hover:bg-gray-400">
-                  Github
+        <div className="mt-28 grid grid-cols-2 gap-20 lg:px-16">
+          {projetos.map((projeto, index) => (
+            <div
+              key={index}
+              className="relative pb-11 bg-[#1a1a1f] mb-10 flex flex-col items-center justify-center rounded-lg py-2 hover:scale-105 transition delay-150 duration-150 ease-in-out shadow-2xl overflow-hidden"
+            >
+              <Image
+                src={projeto.imagem}
+                alt={projeto.nome}
+                width={400}
+                height={400}
+                className="mt-8 w-[600px]"
+              />
+              <p className="text-white text-xl font-bold mt-10">{projeto.nome}</p>
+
+              <div className="flex gap-4 mt-6">
+                <Link href={projeto.githubLink} target="_blank">
+                  <button className="py-2 px-4 bg-gray-300 font-semibold text-black rounded-lg hover:bg-gray-400">
+                    Github
+                  </button>
+                </Link>
+                <Link href={projeto.liveDemoLink} target="_blank">
+                  <button className="py-2 px-4 bg-green-500 text-black font-semibold rounded-lg hover:bg-green-600">
+                    Live Demo
+                  </button>
+                </Link>
+
+                <button
+                  onClick={() => handleOpenModal(projeto)}
+                  className="py-2 px-4 text-white rounded-lg font-bold bg-slate-800 hover:text-[#B38000] "
+                >
+                  Mais Informações
                 </button>
-              </Link>
-              <Link href={projeto.liveDemoLink} target="_blank">
-                <button className="py-2 px-4 bg-green-500 text-black font-semibold rounded-lg hover:bg-green-600">
-                  Live Demo
-                </button>
-              </Link>
-              
-              <button
-                onClick={() => handleOpenModal(projeto)}
-                className="py-2 px-4 text-white rounded-lg font-bold bg-slate-800 hover:text-[#B38000] "
-              >
-                Mais Informações
-              </button>
               </div>
 
-            
-          </div>
-        ))}
-      </div>
 
-      {selectedProject && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
-          <div className="bg-[#1a1a1f] p-8 rounded-lg w-96 relative">
-            <button
-              onClick={handleCloseModal}
-              className="absolute top-2 right-2 text-white font-light hover:text-gray-700"
-            >
-              ✕
-            </button>
-            <h2 className="text-2xl font-bold mb-4">
-              {selectedProject.nome}
-            </h2>
-            <Image
-              src={selectedProject.imagem}
-              alt={selectedProject.nome}
-              width={300}
-              height={200}
-            />
-            <p className="mt-4 text-white font-light">
-              {selectedProject.descricao}
-            </p>
-          </div>
+            </div>
+          ))}
         </div>
-      )}
-    </section>
-    <div className="mt-10"></div>
-    <Footer/>
+
+        {selectedProject && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
+            <div className="bg-[#1a1a1f] p-8 rounded-lg w-96 relative">
+              <button
+                onClick={handleCloseModal}
+                className="absolute top-2 right-2 text-white font-light hover:text-gray-700"
+              >
+                ✕
+              </button>
+              <h2 className="text-2xl font-bold mb-4">
+                {selectedProject.nome}
+              </h2>
+              <Image
+                src={selectedProject.imagem}
+                alt={selectedProject.nome}
+                width={300}
+                height={200}
+              />
+              <p className="mt-4 text-white font-light">
+                {selectedProject.descricao}
+              </p>
+            </div>
+          </div>
+        )}
+      </section>
+      <div className="mt-10"></div>
+      <Footer />
     </div>
-    
+
   );
 }
 export default Home; 
