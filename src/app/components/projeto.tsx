fd015a0158855c14331ae881fef407c548636ab6
Pedro Lima <pedromendeslima2016@gmail.com>
2024-10-21 21:37:36 -0300

    ajustando imagens

--- src/app/components/projeto.tsx
+++ src/app/components/projeto.tsx
@@ -11,7 +11,7 @@
   export const projetos: Projeto[] = [
     {
       nome: "Verbalize",
-      imagem: "/Navbar.png",
+      imagem: "/verbalize.png",
       descricao: "Este é o projeto Verbalize. ele é um projeto feito em grupo com o intuito de ser uma plataforma de chat bidirecional onde você ensina sua linguagem enquanto aprende a do outro. as linguagens que eu usei para fazer o Front-End dele foram: React | Next, utilizando MySQL, Typescript e Tailwind CSS.",
       githubLink: "https://github.com/uPedroLima11/Next_Verbalise_Frontend",
       liveDemoLink: "https://verbalize-senac.vercel.app",
@@ -25,14 +25,14 @@
     },
     {
       nome: "Projeto3",
-      imagem: "/Navbar.png",
+      imagem: "/verbalize.png",
       descricao: "Projeto 3 explora integrações de APIs.",
       githubLink: "https://github.com/projeto3",
       liveDemoLink: "https://projeto3-demo.com",
     },
     {
       nome: "Projeto4",
-      imagem: "/Navbar.png",
+      imagem: "/verbalize.png",
       descricao: "Projeto 4 é uma aplicação completa com Next.js.",
       githubLink: "https://github.com/projeto4",
       liveDemoLink: "https://projeto4-demo.com",
