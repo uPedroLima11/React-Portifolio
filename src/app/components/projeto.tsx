3f7e6278cae805daefe5af9a768a176226cc75a0
Pedro Lima <pedromendeslima2016@gmail.com>
2024-10-23 02:03:55 -0300

    projeto3 adicionado

--- src/app/components/projeto.tsx
+++ src/app/components/projeto.tsx
@@ -16,20 +16,24 @@ export const projetos: Projeto[] = [
     githubLink: "https://github.com/uPedroLima11/Next_Verbalise_Frontend",
     liveDemoLink: "https://verbalize-senac.vercel.app",
   },
+
+  {
+    nome: "ElectroHub *",
+    imagem: "/projeto3.png",
+    descricao: "*Este site que desenvolvi é uma Loja de venda de eletrônicos, oferecendo uma ampla variedade de produtos para os consumidores. O design é moderno e responsivo, garantindo uma navegação intuitiva em dispositivos móveis e desktops. Utilizei as tecnologias React, Next.js, TypeScript e Tailwind CSS para construir o projeto. Meu foco foi proporcionar uma experiência de interface atrativa, eficiente e otimizada. A combinação dessas ferramentas resulta em um site rápido e fácil de usar.*",
+    githubLink: "https://github.com/uPedroLima11/Next_Loja",
+    liveDemoLink: "https://next-loja-nine.vercel.app",
+  },
+
+
   {
     nome: "Sabor e Saúde",
     imagem: "/projeto2.png",
-    descricao: "Esse é um Site de Receitas desenvolvido no 2º semestre da faculdade utilizando apenas html e css. a pagina em si é um site de receitas que tem o intuito de ajudar as pessoas que desejam emagrecer ou apenas ter uma boa saude. com isso o site fornece diversas receitas para ajudar a quem quer emagrecer",
+    descricao: "Este é um Site de Receitas desenvolvido no 2º semestre da faculdade utilizando apenas html e css. a pagina em si é um site de receitas que tem o intuito de ajudar as pessoas que desejam emagrecer ou apenas ter uma boa saude. com isso o site fornece diversas receitas para ajudar a quem quer emagrecer",
     githubLink: "https://github.com/uPedroLima11/Site-Receitas",
     liveDemoLink: "https://site-receitas-pedro.vercel.app",
   },
-  {
-    nome: "Projeto3 * Em Desenvolvimento *",
-    imagem: "/verbalize.png",
-    descricao: "* Em Desenvolvimento *",
-    githubLink: "https://github.com/projeto3",
-    liveDemoLink: "https://projeto3-demo.com",
-  },
+  
   {
     nome: "Projeto4 * Em Desenvolvimento *",
     imagem: "/verbalize.png",
